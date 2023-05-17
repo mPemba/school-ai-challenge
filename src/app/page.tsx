61333fc27b05f4929669d0549a4698f30fc11ba9
mPemba <michael.j.say@gmail.com>
2023-05-17 16:37:12 -0600

    simple UI built

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <h1 className="text-4xl font-bold text-gray-900">SchoolAI Challenge</h1>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          HOME
-        </div>
+        <input
+          className="w-full h-12 px-4 py-2 text-gray-700 my-40 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:border-gray-500"
+          type="text"
+          placeholder="Ask a question"
+        />
       </div>
     </main>
   )
