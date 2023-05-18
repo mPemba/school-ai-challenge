5875ddf09cd23ecea7bbaaebf5424d57b178c73e
mPemba <michael.j.say@gmail.com>
2023-05-18 11:37:10 -0600

    setting typeface and adjusting styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import '../styles/tailwind.css'
 import { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return (<Component {...pageProps} />)
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
